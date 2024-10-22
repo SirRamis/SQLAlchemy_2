0376cea1fade880629232e3a626161b98c2eab03
Ramis <sir.ramis@mail.ru>
2024-10-22 18:05:38 +0300

    add 3

---
+++ 3.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from dotenv import load_dotenv
+import os
+
+# Загрузка переменных окружения из .env файла
+load_dotenv()
+print(os.getenv("DB_DRIVER"))  # Должно вывести "postgresql+psycopg2"
+print(os.getenv("DB_PORT"))    # Должно вывести "5432"
+
+try:
+    url = URL.create(
+        drivername=os.getenv("DB_DRIVER"),
+        username=os.getenv("DB_USERNAME"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME")
+    )
+
+    engine = create_engine(url)
+    connection = engine.connect()
+    print("Подключение успешно!")
+
+except Exception as e:
+    print(f"Ошибка подключения: {e}")
